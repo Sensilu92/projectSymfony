d9b5fd0aaae7b399ae6f27ea5a242e9585a0b4fd
admin <admin@172.16.12.32>
2013-02-21 00:28:47 +0100

    -Modification du type telephone_contact en string de l'entity client et sa doctrine dans le bundle AdministrateurBundle. -Persistance reussi pour l'ajout de client et de projet.

--- src/Suiviprojet/AdministrateurBundle/Controller/AdministrateurController.php
+++ src/Suiviprojet/AdministrateurBundle/Controller/AdministrateurController.php
@@ -12,8 +12,44 @@ class AdministrateurController extends Controller {
     
     public function creationCompteClientAction(){
         
-        $clients =  $this->getDoctrine()->getRepository("SuiviprojetAdministrateurBundle:Client")->findAll();
-        return $this->render('SuiviprojetAdministrateurBundle:Admin:creationCompteClient.html.twig', array('clients'=>$clients));
+       // $clients =  $this->getDoctrine()->getRepository("SuiviprojetAdministrateurBundle:Client")->findAll();
+        //return $this->render('SuiviprojetAdministrateurBundle:Admin:creationCompteClient.html.twig', array('clients'=>$clients));
+        
+        $clients = new \Suiviprojet\AdministrateurBundle\Entity\Client();
+      
+        
+        // récupération la requête
+        $request = $this->get('request');
+        // vérifie qu'elle est de type POST
+       
+        if ($request->getMethod() == 'POST') {
+//             
+            // la variable $client contient les valeurs entrées dans le formulaire 
+
+           $client->setNomContact($request->get('nomContact'));
+           $client->setSociete($request->get('societe'));
+           $client->setPrenomContact($request->get('prenomContact'));
+           $client->setNomContact($request->get('telephoneContact'));
+           $client->setIdentifiantconnection($request->get('Identifiantconnection'));
+           $client->setPasswordconnection($request->get('Passwordconnection'));
+           
+           var_dump($client);
+            
+                // On l'enregistre notre objet $client dans la base de données
+                $entityManager = $this->getDoctrine()->getEntityManager();
+                $entityManager->persist($client);
+                $entityManager->flush();
+
+                // redirige vers la page de visualisation du client nouvellement créé
+              //  return $this->redirect($this->generateUrl('client_succes'));
+            }
+        
+       
+
+        return $this->render('SuiviprojetAdministrateurBundle:Admin:creationCompteClient.html.twig', array()
+                );
+    
+
     }
     
 }
