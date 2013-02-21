8d5208e02b06966cb72c2832c24730862f9527a6
Ndongala <sensilu92@gmail.com>
2013-02-21 21:11:24 +0100

    -Suppression du controller et de la vue index par défaut -Modification des routes et du controller -Modification des vues et suppression des vues superflu

--- src/Suiviprojet/AdministrateurBundle/Controller/AdministrateurController.php
+++ src/Suiviprojet/AdministrateurBundle/Controller/AdministrateurController.php
@@ -1,54 +1,82 @@
 <?php
 
 namespace Suiviprojet\AdministrateurBundle\Controller;
-
+//use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Suiviprojet\AdministrateurBundle\Entity\Client;
+use Suiviprojet\AdministrateurBundle\Entity\Projet;
+
 
 class AdministrateurController extends Controller {
 
     public function creationProjetAction(){
-        return $this->render('SuiviprojetAdministrateurBundle:Admin:creationProjet.html.twig', array());
+         $projet = new Projet();
+ 
+        $form = $this->createFormBuilder($projet)
+                ->add('nom', 'text')
+                ->add('clientclient', 'entity',array('class'=>'SuiviprojetAdministrateurBundle:Client','property'=>'societe'))
+                ->add('dateDebut', 'date')
+                ->add('dateFin', 'date')
+                ->getForm();
+                
+        var_dump($projet);
+
+        $request = $this->get('request');
+
+        if ($request->getMethod() == 'POST') {
+
+            // la variable $client contient les valeurs entrées dans le formulaire 
+           
+            $form->bind($request);
+            if ($form->isvalid()) {
+
+                // On l'enregistre notre objet $client dans la base de données
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($projet);
+                $entityManager->flush();
+            }
+        }
+        return $this->render('SuiviprojetAdministrateurBundle:Admin:creationProjet.html.twig', array('form' => $form->createView(),
+                ));
     }
     
     public function creationCompteClientAction(){
         
-       // $clients =  $this->getDoctrine()->getRepository("SuiviprojetAdministrateurBundle:Client")->findAll();
-        //return $this->render('SuiviprojetAdministrateurBundle:Admin:creationCompteClient.html.twig', array('clients'=>$clients));
-        
-        $clients = new \Suiviprojet\AdministrateurBundle\Entity\Client();
-      
-        
+     $client = new Client();
+        //création du form builder via la méthode du controleur
+        $form = $this->createFormBuilder($client)
+                // ajout des champs qui seront disposé dans le formulaire
+                ->add('societe', 'text')
+                ->add('nomContact', 'text')
+                ->add('prenomContact', 'text')
+                ->add('telephoneContact', 'text')
+                ->add('identifiantconnection', 'text')
+                ->add('passwordconnection', 'password')
+                ->getForm();
         // récupération la requête
         $request = $this->get('request');
         // vérifie qu'elle est de type POST
-       
-        if ($request->getMethod() == 'POST') {
-//             
-            // la variable $client contient les valeurs entrées dans le formulaire 
 
-           $client->setNomContact($request->get('nomContact'));
-           $client->setSociete($request->get('societe'));
-           $client->setPrenomContact($request->get('prenomContact'));
-           $client->setNomContact($request->get('telephoneContact'));
-           $client->setIdentifiantconnection($request->get('Identifiantconnection'));
-           $client->setPasswordconnection($request->get('Passwordconnection'));
-           
-           var_dump($client);
-            
+        if ($request->getMethod() == 'POST') {
+
+            // la variable $client contient les valeurs entrées dans le formulaire 
+            var_dump($client);
+            $form->bind($request);
+            if ($form->isvalid()) {
+
                 // On l'enregistre notre objet $client dans la base de données
-                $entityManager = $this->getDoctrine()->getEntityManager();
+                $entityManager = $this->getDoctrine()->getManager();
                 $entityManager->persist($client);
                 $entityManager->flush();
 
                 // redirige vers la page de visualisation du client nouvellement créé
-              //  return $this->redirect($this->generateUrl('client_succes'));
+                //  return $this->redirect($this->generateUrl('client_succes'));
             }
-        
-       
+        }
 
-        return $this->render('SuiviprojetAdministrateurBundle:Admin:creationCompteClient.html.twig', array()
-                );
-    
+
+        return $this->render('SuiviprojetAdministrateurBundle:Admin:creationCompteClient.html.twig', array('form' => $form->createView(),
+                ));
 
     }
     
